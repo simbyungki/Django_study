3e8562c27ed1f2bd055968443b43592b694c52dc
simbyungki <dung8524@gmail.com>
2020-10-14 13:09:42 +0900

    restart

--- practice/bk_community/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import BKUser
-
-# Register your models here.
-
-class BKUserAdmin(admin.ModelAdmin) : 
-    list_display = ('name', 'email', 'password', 'registered_dttm')
-
-admin.site.register(BKUser, BKUserAdmin)
--- practice/bk_community/webtoon/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('rank_view/', views.rank_view)
-]
